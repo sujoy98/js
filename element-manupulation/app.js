f8bc194694a058cb6e79de4e18e62cf824c316f8
sujoy98 <sujoysarkar951@gmial.com>
2020-09-27 13:45:30 +0530

    update

--- element-manupulation/app.js
+++ element-manupulation/app.js
@@ -1,14 +1,17 @@
-// Create element
+// Create li element
 const li = document.createElement('li');
 
-// Add Class to the element
+// Add Class to the li element
 li.className = 'collection-item';
 
-// Add id to the element
+// Add id to the li element
 li.id = 'new-item';
 
 // Add attribute -> setAttribute accepts two parameter key and value, here title is key and New Item is the value i.e, title = "New Item"
 li.setAttribute('title', 'New Item');
+
+// WE CAN SET ANY ATTRIBUTE
+// li.setAttribute('id', 'new-item');
 
 // Create text node and append into the li we use appendChild, createTextNode is to create the text 'Hello World' inside the li i.e (<li>Hello World</li>)
 li.appendChild(document.createTextNode('Hello World'));
@@ -43,12 +46,12 @@ const newHeading = document.createElement('h2');
 newHeading.id = 'task-title';
 
 // Adding a Text node to the newHeading
-newHeading.appendChild(document.createTextNode('Task-list'));
+newHeading.appendChild(document.createTextNode('Task List'));
 
 // Get the old heading
 const oldHeading = document.getElementById('task-title');
 
-// PARENT, we need to have parent element because we will be using replaceChild function to replace the heading 
+// PARENT, we need to have parent element because we will be using replaceChild function, which we will be calling on the PARENT to replace the heading 
 const cardAction = document.querySelector('.card-action');
 
 // REPLACE, replaceChild accepts two parameter
@@ -70,4 +73,15 @@ lis[0].remove();
 
 // we can also remove using child, ul have li-children
 list.removeChild(lis[3]);
-// 5:09
+
+
+// CLASSES AND ATTRUBUTES
+
+// Inside all the 'li' tag there is a 'a' tag for the cross button, which is the children of the 'li' tag, we can access the children with '.children'
+
+const firstLi = document.querySelector('li');
+
+// we are accessing the children with the index value
+const linkNew = firstLi.children[0];
+
+console.log(linkNew);
