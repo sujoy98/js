9f6f97e020bdb77bfce94db09879a15eee68336d
sujoy98 <sujoysarkar951@gmail.com>
2020-10-04 21:30:22 +0530

    u

--- keyboard-input-EVENTS/app.js
+++ keyboard-input-EVENTS/app.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('form');
 const taskInput = document.getElementById('task');
 const heading = document.querySelector('h5');
+const select = document.querySelector('select');
 
 // After we submit a form we generally need to reset the input area for some new value, we can do that with JS
 taskInput.value = '';
@@ -10,10 +11,35 @@ taskInput.value = '';
 // 'submit' is an anther type of event, its default behaviour is also same as link it redirects to action of the 'form' tag
 form.addEventListener('submit', runEvent);
 
+// Focus, this event gets activated when an input area is clicked and ready to take input from the keyboard
+taskInput.addEventListener('focus', runEvent);
+
+// Blur, opposite to focus, this event gets activated when we exits an input area
+taskInput.addEventListener('blur', runEvent);
+
+// CUT and PASTE events are similar like text cut-paste, inside the input area
+taskInput.addEventListener('cut', runEvent);
+taskInput.addEventListener('paste', runEvent);
+
+// INPUT, it gets activated to any kind of inputs including cut and paste all wull show input event
+// taskInput.addEventListener('input', runEvent);
+
+// Change, this gets activated in changing options
+// select.addEventListener('select', runEvent);
 
 // KEYBOARD EVENTS
 
-form.addEventListener('keydown', runEvent);
+// Keydown, this event gets activated when we press any keyboard key
+// taskInput.addEventListener('keydown', runEvent);
+
+// Keyup, this event gets activated when we release any keyboard key
+// taskInput.addEventListener('keyup', runEvent);
+
+// Keypress, this event gets activated when we press any keyboard key more or less same as keydown
+taskInput.addEventListener('keypress', runEvent);
+
+
+
 
 function runEvent(e){
     console.log(`EventType: ${e.type}`);
@@ -27,6 +53,7 @@ function runEvent(e){
 
     // to get the values from the keydown, we need target
     console.log(e.target.value);
+    
     // form keydown we can change the heading
-    heading.textContent = e.target.value;
+    // heading.textContent = e.target.value;
 }
